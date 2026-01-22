32b59690ec59eaac82ae86a262e4581c443a4717
Samantha Morales <120693377+TxtrisSammi@users.noreply.github.com>
2026-01-22 11:13:39 -0600

    Add files via upload

---
+++ Scripts/Controllers/BikeController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class BikeController : MonoBehaviour
+{
+    // Configuration
+    public float maxSpeed = 2.0f;
+
+    public float turnDistance = 2.0f;
+
+    // Current state data
+
+    public float CurrentSpeed { get; set; }
+
+    public Direction CurrentTurnDirection { get; private set; }
+
+    // State References
+
+    private IBikeState _startState,
+        _stopState,
+        _turnState;
+
+    private BikeStateContext _bikeStateContext;
+
+    void Start()
+
+    {
+        _bikeStateContext = new BikeStateContext(this);
+
+        _startState = gameObject.AddComponent<BikeStartState>();
+
+        _stopState = gameObject.AddComponent<BikeStopState>();
+
+        _turnState = gameObject.AddComponent<BikeTurnState>();
+
+        _bikeStateContext.Transition(_stopState);
+    }
+
+    public void StartBike() =>
+        _bikeStateContext.Transition(_startState);
+
+    public void StopBike() =>
+        _bikeStateContext.Transition(_stopState);
+
+    public void Turn(Direction direction)
+    {
+        CurrentTurnDirection = direction;
+        _bikeStateContext.Transition(_turnState);
+    }
+}
